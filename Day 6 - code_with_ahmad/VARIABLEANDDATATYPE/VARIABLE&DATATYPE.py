cdb08c00440a9047ca93cb1a6409fe65cf413efe
Muhammad Ahmad <133778501+mahmad1999@users.noreply.github.com>
2024-04-22 23:24:14 +0500

    Create VARIABLE&DATATYPE.py
    Day-6 Variable & data type

---
+++ Day 6 - code_with_ahmad/VARIABLEANDDATATYPE/VARIABLE&DATATYPE.py
@@ -0,0 +1,20 @@
+a = complex(8, 2)
+b = "kashi"
+c = True
+d = None
+e = 1.1
+print(a)
+print (b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of d is ", type(d))
+print("The type of e is ", type(e))
+list = [3.4, [4,6], ["banana", "apple"]]
+print(list)
+tuple = (("parrot", "pigeon"), ("lion", "tiger"), (9,7))
+print(tuple)
+dict = {"name":"kashi", "age":25, "canVote":True}
+print(dict)
