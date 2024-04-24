534b641ab53d3783dc5318f079c07f0613b2de38
Muhammad Ahmad <133778501+mahmad1999@users.noreply.github.com>
2024-04-25 00:13:17 +0500

    Calculator

---
+++ Day 8 - code_with_ahmad/Calculator.py
@@ -0,0 +1,16 @@
+a = 56
+b = 3
+addition = a + b
+multiplication = a * b
+subtraction = a - b
+division = a / b
+modulus = a % b
+exponential = a ** b
+floor division = a // b
+print("The value of", a, "+", b, "is =", addition)
+print("The value of", a, "*", b, "is =", multiplication)
+print("The value of", a, "-", b, "is =", subtraction)
+print("The value of", a, "/", b, "is =", subtraction)
+print("The value of", a, "%", b, "is =", modulus)
+print("The value of", a, "**", b, "is =", exponential)
+print("The value of", a, "//", b, "is =", floor division)
