d021c97969194e77202fdcbf4d1e09004df44dd1
Muhammad Ahmad <133778501+mahmad1999@users.noreply.github.com>
2024-05-18 02:18:36 +0500

    Update day 13.py

--- Day 13 - code_with_ahmad/day 13.py
+++ Day 13 - code_with_ahmad/day 13.py
@@ -1,1 +1,33 @@
-print("hello world!")
+# Strings are immutable
+a = "!!Harry!! !!! Harry !!!!!!"
+# print(len(a))
+# print(a)
+# print(a.upper())
+# print(a.lower())
+# print(a.rstrip("!"))
+# print(a.replace("Harry" , "John"))
+# print(a.split(" "))
+blogheading = "introduction to python"
+# print(blogheading.capitalize())
+str1 = "Welcome to the Console !!!"
+#print(len(str1))
+#print(str1.center(50))
+#print(a.count("Harry"))
+#print(str1.endswith("to", 6,10))
+str1 = "He's name hasnain. He is an asshole"
+#print(str1.find("nain"))
+#print(str1.index("nain"))
+#print(str1.isalnum())
+#print(str1.isalpha())
+#print(str1.islower())
+#print(str1.isprintable())
+str2 = "    "
+#print(str2.isspace())
+#print(str1.istitle())
+#print(str1.isupper())
+#print(str1.startswith("He"))
+#print(str1.swapcase())
+#print(str1.title())
+
+
+
