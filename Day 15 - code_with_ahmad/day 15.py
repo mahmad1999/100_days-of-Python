bcf6f2927055e2620ce5ea5f3041478effeaf85a
Muhammad Ahmad <133778501+mahmad1999@users.noreply.github.com>
2024-05-21 00:38:21 +0500

    day 15.py

--- Day 15 - code_with_ahmad/day 15.py
+++ Day 15 - code_with_ahmad/day 15.py
@@ -1,1 +1,19 @@
-print("hello world!")
+import time
+
+tim = time.strftime('%H : %M : %S')
+print(tim)
+
+if tim  <= '12:00:00' and tim >= '5:00:00':
+  print("Good Morning")
+
+elif tim  <= '18:00:00' and tim > '12:00:00':
+  print("Good After Noon")
+
+elif tim  <= '21:00:00' and tim > '18:00:00':
+  print('Good Evening')
+
+elif tim  <= '24:00:00' and tim > '21:00:00':
+  print("Good Night")
+
+else:
+  print("Good Middle Of Night")
