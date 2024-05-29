8df81a7534b5ab51835dac80529a9ba22b5510e3
Muhammad Ahmad <133778501+mahmad1999@users.noreply.github.com>
2024-05-29 23:35:30 +0500

    for loop

--- Day 17 - code_with_ahmad/day 17.py
+++ Day 17 - code_with_ahmad/day 17.py
@@ -1,1 +1,16 @@
-print("hello world!")
+# x = 'python'
+# for i in x:
+  # print(i, end=', ')
+# y = ("red", "yellow", "green", "blue")
+# for i in y:
+#   # print(i)
+#   for c in i:
+#     print(c)
+# for z in range(10):
+#   print(z)
+# for z in range(1, 10):
+#   print(z
+# for z in range(1, 5, 2):
+#   print(z)
+for z in [x*0.1 for x in range(10)]:
+  print(z)
